b2d6621d83a869d158aafb6db6ae8d92c5736584
RNZZL <46511591+RNZZL@users.noreply.github.com>
2019-02-14 07:37:07 +0800

    video

---
+++ DejaWu-master/js/background.js
@@ -0,0 +1,168 @@
+Array.max = function( array ){
+    return Math.max.apply(Math,array);
+};
+
+/**
+ * @return {boolean}
+ */
+function isValidUrl(text) {
+    const valid = /((https?):\/\/)?(([w|W]{3}\.)+)?[a-zA-Z0-9\-\.]{3,}\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?/;
+    return valid.test(text);
+}
+
+chrome.omnibox.onInputChanged.addListener(omnibarHandler);
+chrome.omnibox.onInputEntered.addListener(acceptInput);
+chrome.runtime.onMessage.addListener(handleMessage);
+chrome.runtime.onInstalled.addListener(function(details){
+chrome.storage.local.clear(function() {
+        var error = chrome.runtime.lastError;
+        if (error) {
+            console.error(error);
+        }
+    });
+});//parameter details has no use
+
+function acceptInput(text, disposition) {
+    console.log("Input given!");
+    // disposition: "currentTab", "newForegroundTab", or "newBackgroundTab"
+    if (!isValidUrl(text)) {
+        return;
+    }
+    switch (disposition) {
+    case "currentTab":
+        chrome.tabs.update({url: text});
+        break;
+    case "newForegroundTab":
+        chrome.tabs.create({url: text});
+        break;
+    case "newBackgroundTab":
+        chrome.tabs.create({url: text, active: false});
+        break;
+    }
+}
+
+function updateVideos(array)
+{
+    chrome.storage.local.set({
+        snippets:array//The item named behaviorItems in storage that corresponds to the items.
+    }, function() {
+        console.log("local storage updated");
+        console.log("number of items: " + array.length);
+    });
+}
+
+//request is behaviorItem in content.js
+function handleMessage(request, sender, sendResponse) {
+    let behaviorCodes = ["copy", "select", 'search'];
+    if (behaviorCodes.includes(request.eventtype)) {
+        // get the stored behaviorItems
+        chrome.storage.local.get(
+            ['behaviorItems'],
+            function(result){
+              let bhvItems = result.behaviorItems;
+
+              // Initialize the items if nothing stored yet
+              if (!Array.isArray(bhvItems))
+              {
+                  update('behaviorItems',[]);
+              }
+              // Update the array with the latest item.
+              else{
+                  console.log("request: ", request);
+                  bhvItems.push(request);
+                  update('behaviorItems', bhvItems);
+                  console.log(bhvItems);
+              }
+        });
+    }
+    if (request.eventtype === 'video'){
+        console.log("videoooo");
+        chrome.storage.local.get(
+            ["snippets"],
+            function(result){
+                console.log(result);
+                let videoSnippets = result.snippets;
+                if (!Array.isArray(videoSnippets))
+                {
+                    console.log("heyyy", "heyy");
+                    updateVideos([]);
+                }
+                else{
+                    console.log(request);
+                    videoSnippets.push(request);
+                    updateVideos( videoSnippets);
+                }
+            }
+        )
+    }
+    if (request.eventtype ==="video_play"){
+        chrome.storage.local.get(
+            ["snippets"],
+            function(result){
+                console.log(result);
+                let videoSnippets = result.snippets;
+                if (!Array.isArray(videoSnippets))
+                {
+                    console.log("heyyy", "heyy");
+                    updateVideos([]);
+                    sendResponse(null);
+                }
+                else{
+                    console.log(request);
+                    let videoSnippetsArray=[];//define an Array to send
+                    for (let i=0; i<videoSnippets.length; i++)
+                    {
+                        // TODO: handle same video with different parameters
+                        // TODO: handle visualization of multiple snippets
+                        //console.log(videoSnippets[i].url);
+                        //console.log(request.url);
+                        if(videoSnippets[i].data.split(":")[0] !== videoSnippets[i].data.split(":")[1]){
+                          if (videoSnippets[i].url === request.url){
+                            videoSnippetsArray.push(videoSnippets[i].data);
+                          }
+                       }
+                    }
+                   sendResponse(videoSnippetsArray);
+                }
+            }
+        )
+    }
+    return true;
+}
+
+function omnibarHandler(text, suggest)
+{
+    // Holder for behavior items that contained copied code snippets.
+    let codeSnippets = [];
+    chrome.storage.local.get(
+        ['behaviorItems'],
+        function(result) {
+            //All processing must be included in this callback.
+            let bhvItems = result.behaviorItems;
+            for(let i=0; i<bhvItems.length; i++)
+            {
+                if(bhvItems[i].datatype==="code")
+                {
+                    codeSnippets.push(bhvItems[i]);
+                }
+            }
+            let suggestions = [];
+            //Push suggestions.
+            for(let i=0; i<codeSnippets.length; i++)
+            {
+                //If the current sequence typed is included in a code snippet.
+                if(codeSnippets[i].data.indexOf(text)!==-1)
+                {
+                    //Content is that filled when selected, description that appears.
+                    suggestions.push({content:codeSnippets[i].data,
+                                        description:codeSnippets[i].data});
+                }
+                //Or if the text matches the title above that recorded code.
+                else if(codeSnippets[i].title.indexOf(text!==-1))
+                    suggestions.push({content:codeSnippets[i].data,
+                        description:codeSnippets[i].data});
+            }
+            suggest(suggestions);
+        });
+
+}
---
+++ DejaWu-master/js/content.js
@@ -0,0 +1,166 @@
+let lastVideoTime = 0;
+let lastVideoSnippetStartTime = 0;
+
+function newUrlHandler()
+{
+    let url = document.location.href;
+    let query;
+    if (url.includes("google.com")) {
+        const regex = /(?<=q=).*?(?=&)/s;
+        if (url.match(regex) !== null) {
+            query = url.match(regex)[0].replace(/\+/g, ' ');
+            let behaviorItem = makeBehaviorItem("search", query);
+            chrome.runtime.sendMessage(behaviorItem, (response) => {
+                console.log("Message Response: ", response); //Response is undefined.
+            });
+        }
+    }
+    //TODO: collect url history here.
+}
+
+function highlightHandler(e)
+{
+    //Get selected text.
+    let content = extractSelectionText();
+    //If no content has been selected, ignore.
+    if(content==="")
+        return;
+    let bhvItm = makeBehaviorItem("select",content);
+    chrome.runtime.sendMessage(bhvItm, (response) => {
+        console.log("Message Response: ", response); //Response is undefined.
+    });
+}
+
+function clipboardHandler(e)
+{
+    //Check for text selection.
+    let content = extractSelectionText();
+    if(content==="")
+        return;
+    //Store the behavior duplicate in the content of the text selected.
+    let behaviorItem = makeBehaviorItem("copy", content);
+    /*
+        The superceding section concerns the detection of a html code element.
+     */
+    //A cursor that tracks the hierarchy of elements to see if a <code> tag is present.
+    let cur = e.target;
+    while(cur.tagName.toLowerCase()!=="code" && (cur=cur.parentNode)!==document.body);
+    //If the highest element tracked by the cursor can be ascribed to <code>.
+    if(cur.tagName.toLowerCase()==="code")
+    {
+        //Add code indication to datatype.
+        behaviorItem["datatype"] = "code";
+    }
+    //TODO: add case for image url
+    //Send message to the background with modification requirements. The time recorded
+    //of the copy event would be that of the highlighted.
+    chrome.runtime.sendMessage(behaviorItem, (response) => {
+        console.log("Message Response: ", response); //Response is undefined.
+    });
+}
+
+/**
+ * This function makes a customary representation of a user's browsing behavior.
+ * It is a map with the keys "type", the type of browsing event, "data", the content
+ * correlated with the event, "time", time of this event, "title", the title of the
+ * page, and "url", the hyperlink to that site.
+ * @param event_type event type
+ * @param content The content associated with this event.
+ * @returns {{data: *, time: Date, type: *, title: string, url: string}}
+ */
+function makeBehaviorItem(event_type, content)
+{
+    return {
+        "eventtype": event_type,
+        "time": new Date(),
+        "url": document.location.href,
+        "title": document.title,
+        "data": content,
+    };
+}
+
+/**
+ * Extract selected text. Returns "" if no text selected.
+ * @returns {string}
+ */
+function extractSelectionText()
+{
+    //Text selected.
+    let sel = window.getSelection();
+    if (sel)
+    {
+        //Highlighted content;
+        //If no text has been selected, and mouseup was simply trigger by itself.
+        return sel.toString();
+    }
+}
+
+function drawMarker(time_pair,duration){
+    let $blueBar =$(blueProgressBar);
+    let ratio = time_pair[1]/duration-time_pair[0]/duration,
+        propValue= `scaleX(${ratio})`;
+    $blueBar.css('left',((time_pair[0]/duration)*100)+'%');
+    $blueBar.css('transform',propValue);
+    $('div.ytp-play-progress.ytp-swatch-background-color:not(.blueProgress)').before($blueBar);
+}
+
+
+$(document).arrive('video', {existing:true}, function(v){
+    let videoObj = v;
+    let behaviorItem;
+    behaviorItem = makeBehaviorItem("video_play");
+    chrome.runtime.sendMessage(behaviorItem, (response) => {
+      for(let i=0;i<=response.length-1;i++){
+        console.log("Message Response: ", response[i]); //Response is undefined.
+        drawMarker(response[i].split(":"), videoObj.duration);
+      }
+    });
+
+    videoObj.ontimeupdate = function(){
+        // if there is a big gap between the current play time and the last play time,
+        // the user has skipped/rewind the video
+    if(isVideoAnAD()===false){
+          if (Math.abs(videoObj.currentTime - lastVideoTime) > 5) {
+              console.log("snippet:" + lastVideoSnippetStartTime + " ---  " + lastVideoTime);
+              behaviorItem = makeBehaviorItem("video", lastVideoSnippetStartTime + ":" + lastVideoTime)
+              lastVideoSnippetStartTime = videoObj.currentTime;
+              chrome.runtime.sendMessage(behaviorItem, (response) => {
+                  console.log("Message Response: ", response); //Response is undefined.
+              });
+              //TODO: handle rewind event.
+          }
+          lastVideoTime = videoObj.currentTime;//
+        }
+    };
+
+    videoObj.onpause=function(){
+        console.log("paused");
+    };
+
+    videoObj.onended= function () {
+        console.log("ended");
+    };
+
+    window.onbeforeunload=function(e){
+      console.log("closed");
+      behaviorItem = makeBehaviorItem("video", lastVideoSnippetStartTime + ":" + lastVideoTime);
+      chrome.runtime.sendMessage(behaviorItem, (response) => {
+          console.log("Message Response: ", response); //Response is undefined.
+      });
+    }
+});
+
+function isVideoAnAD(){
+  if($(".ytp-play-progress").css("background-color") === "rgb(255, 204, 0)"){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+
+
+
+document.addEventListener('copy', clipboardHandler);
+document.addEventListener('mouseup', highlightHandler);
+window.addEventListener("load", newUrlHandler);
